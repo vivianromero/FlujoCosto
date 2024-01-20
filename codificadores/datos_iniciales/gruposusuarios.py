dc7ce50e3649dc88f49f042298b2c10a040fa2c9
vromerobuchillon@gmail.com <vromerobuchillon@gmail.com>
2024-01-20 08:24:12 -0500

    Arreglos en modelos

--- codificadores/datos_iniciales/gruposusuarios.py
+++ codificadores/datos_iniciales/gruposusuarios.py
@@ -7,10 +7,10 @@ def init_data(apps, schema_editor):
     Grupos = apps.get_model("auth", "Group")
 
     grupos = [
-        CategoriaVitola(id=1, name='Administración'),
-        CategoriaVitola(id=2, name='Operador Flujo'),
-        CategoriaVitola(id=3, name='Operador Costo'),
-        CategoriaVitola(id=4, name='Consultor'),
+        Grupos(id=1, name='Administración'),
+        Grupos(id=2, name='Operador Flujo'),
+        Grupos(id=3, name='Operador Costo'),
+        Grupos(id=4, name='Consultor'),
     ]
 
     db_alias = schema_editor.connection.alias
