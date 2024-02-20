e3ed03b642acd2915286c16d8d1c3dab99aff13a
vromerobuchillon@gmail.com <vromerobuchillon@gmail.com>
2024-02-20 11:08:36 -0500

    Arreglos en modelo conexion a base de datos externas

--- configuracion/models.py
+++ configuracion/models.py
@@ -24,12 +24,10 @@ class ConexionBaseDato(models.Model):
     password = models.CharField(max_length=250, verbose_name=_("Password"))
     host = models.CharField(max_length=250, verbose_name=_("Host"))
     port = models.CharField(max_length=100, verbose_name=_("Port"))
-    idunidadcontable = models.ForeignKey(UnidadContable, on_delete=models.PROTECT, verbose_name="UEB")
+    idunidadcontable = models.ForeignKey(UnidadContable, on_delete=models.PROTECT, verbose_name="UEB", unique=True)
 
     class Meta:
         db_table = 'cfg_conexionasedato'
-        unique_together = (('database_name', 'idueb'),)
-
 
 class ConsecutivoDocumento(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
