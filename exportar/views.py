6b28ef84fe1ec289583564aafa42923c0b0ca00f
vromerobuchillon@gmail.com <vromerobuchillon@gmail.com>
2024-03-03 10:11:25 -0500

    Exportacoón de datos

--- exportar/views.py
+++ exportar/views.py
@@ -1,19 +1,26 @@
 # views.py
-from django.http import HttpResponse
-from django.template.loader import render_to_string
+import json
+
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+
+from utiles.utils import obtener_version, codificar
+
 
 @login_required
 def uc_exportar(request):
     # Lógica para generar el contenido del archivo (por ejemplo, usando pandas)
     contenido_archivo = "Contenido del archivo a exportar"
 
+    json_info = json_info('UC')
+
+
     # Configuración de la respuesta HTTP para la descarga del archivo
-    response = HttpResponse(content_type='text/csv')
-    response['Content-Disposition'] = 'attachment; filename="archivo_exportado_uc.csv"'
+    # response = HttpResponse(content_type='text/csv')
+    # response['Content-Disposition'] = 'attachment; filename="archivo_exportado_uc.csv"'
 
     # Escribir el contenido del archivo en la respuesta
-    response.write(contenido_archivo)
+    # response.write(contenido_archivo)
     return response
 
 @login_required
@@ -34,6 +41,8 @@ def umc_exportar(request):
     # Lógica para generar el contenido del archivo (por ejemplo, usando pandas)
     contenido_archivo = "Contenido del archivo a exportar"
 
+    app_version_file = open('version', 'r')
+
     # Configuración de la respuesta HTTP para la descarga del archivo
     response = HttpResponse(content_type='text/csv')
     response['Content-Disposition'] = 'attachment; filename="archivo_exportado_umc.csv"'
@@ -41,3 +50,14 @@ def umc_exportar(request):
     # Escribir el contenido del archivo en la respuesta
     response.write(contenido_archivo)
     return response
+
+def json_info(opcion):
+    version = obtener_version()
+    check_sum = codificar(opcion + version)
+    dicc_valid = {'opcion': opcion,
+                  'version': version,
+                  'check_sum': check_sum}
+    encoder = json.encoder.JSONEncoder()
+    # json_info = encoder.encode(dicc_valid)
+    return encoder.encode(dicc_valid)
+
