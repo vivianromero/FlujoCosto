83b2d2e27b0c0ee81dafb3b16acfc68e361bea4c
vromerobuchillon@gmail.com <vromerobuchillon@gmail.com>
2024-03-10 17:58:33 -0400

    Exportación de datos

--- app_apiversat/views.py
+++ app_apiversat/views.py
@@ -4,23 +4,25 @@ from django.utils.translation import gettext_lazy as _
 from codificadores.models import UnidadContable
 from cruds_adminlte3.utils import crud_url_name
 from utiles.decorators import adminempresa_required
+from utiles.utils import message_error, message_success
 from .functionapi import getAPI
-from utiles.utils import message_success, message_error
 
 
 @adminempresa_required
 def UC_Versat(request):
     try:
+        title_error = _("Couldn't update")
+        text_error = _('Connection error')
         response = getAPI('unidad')
-        if response.status_code == 200:
+        if response and response.status_code == 200:
             data_json = response.json()  # Assuming the response is in JSON format
             datos = [UnidadContable(codigo=item['codigo'], nombre=item['nombre'], activo=item['activo']) for item in data_json]
             UnidadContable.objects.bulk_update_or_create(datos, ['nombre','activo'], match_field='codigo')
-            message_success(request=request, title=_('Data were successfully update'), text=_('Success update'))
+            message_success(request=request, title=_("Success"), text=_('Data importation was successful'))
         else:
-            message_error(request=request, title=_("Couldn't update"), text=_('Connection error'))
+            message_error(request=request, title=title_error, text=text_error)
     except Exception as e:
-        message_error(request=request, title=_("Couldn't update"), text=_('Connection error'))
+        message_error(request=request, title=title_error, text=text_error)
 
     return redirect(crud_url_name(UnidadContable, 'list', 'app_index:codificadores:'))
 
--- exportar/views.py
+++ exportar/views.py
@@ -1,54 +1,35 @@
 # views.py
 import json
+import os
+import tarfile
 
-from django.contrib.auth.decorators import login_required
+from django.core import serializers
 from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.utils.translation import gettext_lazy as _
 
+from codificadores.models import UnidadContable, Medida, MedidaConversion, MarcaSalida
+from cruds_adminlte3.utils import crud_url_name
+from utiles.decorators import adminempresa_required
+from utiles.utils import message_success
 from utiles.utils import obtener_version, codificar
 
 
-@login_required
+@adminempresa_required
 def uc_exportar(request):
-    # Lógica para generar el contenido del archivo (por ejemplo, usando pandas)
-    contenido_archivo = "Contenido del archivo a exportar"
+    return crear_export_file(request, 'UC', UnidadContable)
 
-    json_verify = json_info('UC')
-
-    # Configuración de la respuesta HTTP para la descarga del archivo
-    response = HttpResponse(content_type='text/json')
-    response['Content-Disposition'] = 'attachment; filename="archivo_exportado_uc.json"'
-
-    # Escribir el contenido del archivo en la respuesta
-    response.write(json_verify)
-    return response
-
-@login_required
+@adminempresa_required
 def um_exportar(request):
-    # Lógica para generar el contenido del archivo (por ejemplo, usando pandas)
-    contenido_archivo = "Contenido del archivo a exportar"
+    return crear_export_file(request, 'UM', Medida)
 
-    # Configuración de la respuesta HTTP para la descarga del archivo
-    response = HttpResponse(content_type='text/csv')
-    response['Content-Disposition'] = 'attachment; filename="archivo_exportado_um.csv"'
-
-    # Escribir el contenido del archivo en la respuesta
-    response.write(contenido_archivo)
-    return response
-
-@login_required
+@adminempresa_required
 def umc_exportar(request):
-    # Lógica para generar el contenido del archivo (por ejemplo, usando pandas)
-    contenido_archivo = "Contenido del archivo a exportar"
+    return crear_export_file(request, 'UMC', MedidaConversion)
 
-    app_version_file = open('version', 'r')
-
-    # Configuración de la respuesta HTTP para la descarga del archivo
-    response = HttpResponse(content_type='text/csv')
-    response['Content-Disposition'] = 'attachment; filename="archivo_exportado_umc.csv"'
-
-    # Escribir el contenido del archivo en la respuesta
-    response.write(contenido_archivo)
-    return response
+@adminempresa_required
+def ms_exportar(request):
+    return crear_export_file(request, 'MS', MarcaSalida)
 
 def json_info(opcion):
     version = obtener_version()
@@ -56,7 +37,45 @@ def json_info(opcion):
     dicc_valid = {'opcion': opcion,
                   'version': version,
                   'check_sum': check_sum}
+    return dicc_valid
+
+def crear_export_file(request, opcion, modelo):
+
+    dicc_verify = json_info(opcion)
+    file_path = "staticfiles"
+
+    json_data = serializers.serialize("json", modelo.objects.all())
+    check_sum_data = codificar(json_data)
+    dicc_verify['check_sum_data'] = check_sum_data
+
+    if len(json_data) <= 2:
+        message_success(request=request, title=_("Warning"), text=_('There are not data to export'))
+        return redirect(crud_url_name(modelo, 'list', 'app_index:codificadores:'))
+
     encoder = json.encoder.JSONEncoder()
-    # json_info = encoder.encode(dicc_valid)
-    return encoder.encode(dicc_valid)
+    json_verify = encoder.encode(dicc_verify)
+
+    filenameverify = 'verify_' + dicc_verify['opcion'] + '.json'
+    ruta_archivo_verify = os.path.join(file_path, 'download', filenameverify)
+    fichero_json = open(ruta_archivo_verify, "w+")
+    fichero_json.write(json_verify)
+    fichero_json.close()
+
+    filenamedata = 'data_'+ dicc_verify['opcion'] + '.json'
+    ruta_archivo_data = os.path.join(file_path, 'download', filenamedata)
+    file_json_data = open(ruta_archivo_data, "w+")
+    file_json_data.write(json_data)
+    file_json_data.close()
+
+    filename = "Exportando_" + dicc_verify['opcion'] + '_' + dicc_verify['version'].replace('.', '-') + ".tar.gz"
+    ruta_archivo = os.path.join(file_path, 'download', filename)
+    with tarfile.open(ruta_archivo, mode='w:bz2') as out:
+        out.add(ruta_archivo_verify, "json_verify")
+        out.add(ruta_archivo_data, "json_data")
+
+
+    # Configuración de la respuesta HTTP para la descarga del archivo
+    response = HttpResponse(open(ruta_archivo, 'rb'), content_type='application/gzip')
+    response['Content-Disposition'] = f'attachment; filename={filename}'
+    return response
 
