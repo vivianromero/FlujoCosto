90888795dc295f888fd35a77347b5e0dfb1499b3
Vivian Romero Buchillón <vromerobuchillon@gmail.com>
2024-05-21 15:18:43 -0400

    Documentos del Versat

--- flujo/views.py
+++ flujo/views.py
@@ -1,33 +1,21 @@
-from django.shortcuts import render
+from django.db.models import F
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy as _
 
+from app_apiversat.functionapi import getAPI
 from app_index.views import CommonCRUDView
+from codificadores.models import Departamento
+from cruds_adminlte3.utils import crud_url_name
 from flujo.filters import DocumentoFilter
-from flujo.forms import DepartamentoDocumentosForm, DocumentoForm, DocumentoFormFilter
+from flujo.forms import DocumentoForm
 from flujo.models import Documento
 from flujo.tables import DocumentoTable, DocumentosVersatTable
-from codificadores.tables import CentroCostoTable
-from codificadores.models import CentroCosto, Departamento
-from app_versat.inventario import InvDocumento, InvDocumentocta, InvDocumentogasto
-from app_versat.serializersdata import InvDocumentoSerializer, InvDocumentogastoSerializer
-
-from django.urls import reverse_lazy
-from django.http import HttpResponseRedirect
-from django_htmx.http import HttpResponseLocation
-import requests
-from dynamic_db_router.router import THREAD_LOCAL
-from dynamic_db_router import in_database
-from configuracion.models import ConexionBaseDato
-from configuracion import ChoiceSystems
 from utiles.utils import message_error
-from django.utils.translation import gettext_lazy as _
-from django.shortcuts import redirect
-from cruds_adminlte3.utils import crud_url_name
-from django.db import connections
-from django.conf import settings
-import datetime
-from .forms import DepartamentoDocumentosForm
-from .models import DocumentoOrigenVersat, DocumentoVersatRechazado
-from app_apiversat.functionapi import getAPI
+from .models import *
+from .utils import ids_documentos_versat_procesados
+
+
 # Create your views here.
 
 # ------ Documento / CRUD ------
@@ -71,7 +59,6 @@ class DocumentoCRUD(CommonCRUDView):
     paginate_by = 5
     table_class = DocumentoTable
 
-
     def get_filter_list_view(self):
         view = super().get_filter_list_view()
 
@@ -84,18 +71,19 @@ class DocumentoCRUD(CommonCRUDView):
 
                 tableversat = None
                 if self.dep:
-                    datostableversat = dame_documentos_versat(self.request)
+                    datostableversat = dame_documentos_versat(self.request, self.dep)
                     tableversat = DocumentosVersatTable(datostableversat)
 
                 context.update({
                     'filter': True,
-                    'url_docversat': reverse_lazy(crud_url_name(Documento, 'list', 'app_index:flujo:')),
+                    'url_docversat': reverse_lazy(
+                        crud_url_name(Documento, 'list', 'app_index:flujo:')) if self.dep else None,
                     'tableversat': tableversat if tableversat else None,
                     "hx_get": reverse_lazy(crud_url_name(Documento, 'list', 'app_index:flujo:')),
                     "hx_target": '#main_content_swap',
                 })
                 return context
-            
+
             def get_queryset(self):
                 queryset = super().get_queryset()
                 self.dep = self.request.GET.get('departamento', None)
@@ -114,19 +102,33 @@ class DocumentoCRUD(CommonCRUDView):
 
         return OFilterListView
 
-def dame_documentos_versat(request):
+
+def dame_documentos_versat(request, dpto):
     unidadcontable = request.user.ueb
-    departamento = request.GET.get('departamento', None)
+
     title_error = _("Couldn't update")
     text_error = _('Connection error')
 
     try:
-        response = getAPI('documentogasto', {'fecha_desde': '2023-01-18', 'fecha_hasta': '2023-01-18'})
+        # TODO implemetar la funcionalidad para que devuelva la fecha de inicio de procesamiento, la fecha de procesamiento y
+        # el mes de procesamiento
+        fecha_inicio_procesamiento = '2023-01-18'
+        fecha_procesamiento = '2023-01-25'
+        fecha_mes_procesamiento = '2023-01-01'
+        dpto = Departamento.objects.get(pk=dpto)
+        param = {'fecha_desde': fecha_inicio_procesamiento,
+                 'fecha_hasta': fecha_procesamiento,
+                 'unidad': unidadcontable.codigo,
+                 'centro_costo': dpto.centrocosto.clave
+                 }
+        response = getAPI('documentogasto', param)
 
         if response and response.status_code == 200:
             datos = response.json()['results']
+            ids = ids_documentos_versat_procesados(fecha_mes_procesamiento, fecha_procesamiento, dpto, unidadcontable)
+            datos = list(filter(lambda x: x['iddocumento'] not in ids, datos))
             return datos
+
     except Exception as e:
         message_error(request=request, title=title_error, text=text_error)
         return redirect(crud_url_name(Documento, 'list', 'app_index:flujo:'))
-
